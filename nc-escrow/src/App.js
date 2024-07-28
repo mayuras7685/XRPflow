529f7706319d0d6769a8e660abbca431fef5bf04
mayuras7685 <mayurasodara@gmail.com>
2024-07-29 04:57:07 +0530

    feat: Landing page updated

--- nc-escrow/src/App.js
+++ nc-escrow/src/App.js
@@ -1,74 +1,91 @@
 // Import necessary libraries
-import React from "react";
-import { Button } from "antd";
-import { XummPkce } from "xumm-oauth2-pkce";
-import "./App.css";
-import { motion } from "framer-motion";
-import { useNavigate } from "react-router-dom";
+import React from "react"
+import { Button } from "antd"
+import { XummPkce } from "xumm-oauth2-pkce"
+import "./App.css"
+import { motion } from "framer-motion"
+import { useNavigate } from "react-router-dom"
 
 // Define the landing page component
 function LandingPage() {
-  const navigate = useNavigate();
+  const navigate = useNavigate()
 
   const signedInHandler = (authorized) => {
     // Assign to global,
     // please don't do this but for the sake of the demo it's easy
-    window.sdk = authorized.sdk;
-    navigate("/dashboard");
-  };
+    window.sdk = authorized.sdk
+    navigate("/dashboard")
+  }
 
   const onConnect = async () => {
     var auth = new XummPkce("4dd5496e-2d69-4741-b242-f8607d415e72", {
       implicit: true,
-    });
+    })
 
     auth.on("error", (error) => {
-      console.log("error", error);
-    });
+      console.log("error", error)
+    })
 
     auth.on("success", async () => {
-      console.log("success");
+      console.log("success")
       auth.state().then((state) => {
         if (state.me) {
-          console.log("success, me", JSON.stringify(state.me));
+          console.log("success, me", JSON.stringify(state.me))
         }
-      });
-    });
+      })
+    })
 
     auth.on("retrieved", async () => {
       // Redirect, e.g. mobile. Mobile may return to new tab, this
       // must retrieve the state and process it like normally the authorize method
       // would do
-      console.log("Results are in, mobile flow, process sign in");
+      console.log("Results are in, mobile flow, process sign in")
 
       auth.state().then((state) => {
-        console.log(state);
+        console.log(state)
         if (state) {
-          console.log("retrieved, me:", JSON.stringify(state.me));
+          console.log("retrieved, me:", JSON.stringify(state.me))
           window.wallet_address = state.me.account
-          signedInHandler(state);
+          signedInHandler(state)
         }
-      });
-    });
+      })
+    })
 
-    await auth.authorize();
-  };
+    await auth.authorize()
+  }
 
   return (
-    <div className="bg-gradient-to-r from-sky-200 to-purple-200">
+    <div className="bg-black">
+      <div className="fixed flex items-center justify-center w-full h-full">
+        <video
+          autoPlay
+          loop
+          muted
+          height={"90%"}
+          className="rounded-3xl Home_BG"
+        >
+          <source src="/xrpl.mp4" type="video/mp4" />
+          {/* Add additional source tags for other video formats */}
+        </video>
+      </div>
       <div className="container mx-auto">
         <div className="flex flex-col justify-center items-center h-screen">
-        <motion.h1
+          <motion.h1
             initial={{ opacity: 0, y: -100 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 0.5, duration: 1 }}
-            className="text-black text-5xl font-bold mb-4 relative"
+            className="text-white text-5xl font-bold mb-4 relative Textsh"
           >
             <motion.span
               initial={{ scale: 0 }}
               animate={{ scale: [1.2, 1, 1.2], rotate: [0, 10, -10, 0] }}
-              transition={{ repeatType:'loop', delay: 1, duration: 1, times: [0, 0.2, 0.8, 1] }}
-              className="inline-block relative"
+              transition={{
+                repeatType: "loop",
+                delay: 1,
+                duration: 1,
+                times: [0, 0.2, 0.8, 1],
+              }}
+              className="inline-block relative TextSh"
             >
               X
             </motion.span>
@@ -111,7 +128,7 @@ function LandingPage() {
         </div>
       </div>
     </div>
-  );
+  )
 }
 
-export default LandingPage;
+export default LandingPage
