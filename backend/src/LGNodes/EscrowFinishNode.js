af77a7cb1ed4e9e601fa190dfd7a4c2c9ccee579
mayuras7685 <mayurasodara@gmail.com>
2024-08-12 00:44:11 +0530

    fix

--- backend/src/LGNodes/EscrowFinishNode.js
+++ backend/src/LGNodes/EscrowFinishNode.js
@@ -1,25 +1,23 @@
-
-import { LiteGraph } from "litegraph.js";
-import escrow from "../escrow/escrowmodel";
+import { LiteGraph } from "litegraph.js"
+import escrow from "../escrow/escrowmodel"
 // Define custom node class
 function EscrowFinishNode() {
-  this.color = "#66CC66"; // green
-  this.size = [120, 60];
+  this.color = "#66CC66" // green
+  this.size = [120, 60]
   // Add input trigger port
-  this.addInput("OnFinish", LiteGraph.ACTION);
+  this.addInput("OnFinish", LiteGraph.ACTION)
 }
 
 EscrowFinishNode.prototype.onAction = async function () {
-  if(this.escrowId=='one'){
-    return;
+  if (this.escrowId == "one") {
+    return
   }
   const updatedEscrow = await escrow.findOneAndUpdate(
     { txid: this.escrowId },
     { status: true },
     { new: true } // return the updated document
-  );
+  )
   if (updatedEscrow) {
-  
     this.graph.stop()
   }
 }
@@ -30,9 +28,9 @@ EscrowFinishNode.prototype.onAdded = async function () {
   });*/
 }
 
-EscrowFinishNode.title = "Escrow Finish";
-EscrowFinishNode.desc = "Escrow Finish";
-EscrowFinishNode.title_text_color = "#000000";
+EscrowFinishNode.title = "Escrow Finish"
+EscrowFinishNode.desc = "Escrow Finish"
+EscrowFinishNode.title_text_color = "#000000"
 
 // Register custom node class with LiteGraph and place it under a "Custom" category in the menu
-LiteGraph.registerNodeType("Escrow/EscrowFinish", EscrowFinishNode);
+LiteGraph.registerNodeType("Escrow/EscrowFinish", EscrowFinishNode)
